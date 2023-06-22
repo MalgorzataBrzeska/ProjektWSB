08554743b519c635f21ec658a7e3104b910258e3
MalgorzataBrzeska <137276852+MalgorzataBrzeska@users.noreply.github.com>
2023-06-22 11:22:19 +0200

    gra.java

--- Projekt22/Projekt/src/main/java/gra.java
+++ Projekt22/Projekt/src/main/java/gra.java
@@ -1,10 +1,11 @@
 package gra;
 
 public class WisielecGra {
-    private String slowo;
-    private StringBuilder zgadywaneLitery;
-    private int maxProb;
-    private int bledneProby;
+    private String slowo;  // Słowo do odgadnięcia
+    private StringBuilder zgadywaneLitery; // Odgadnięte litery
+    private int maxProb; // Maksymalna liczba błędnych prób
+    private int bledneProby; // Liczba błędnych prób
+
 
     public WisielecGra(String slowo, int maxProb) {
         this.slowo = slowo.toLowerCase();
@@ -12,11 +13,11 @@ public class WisielecGra {
         zgadywaneLitery = new StringBuilder();
     }
 
-
+// Sprawdza, czy gra się skończyła
     public boolean czyKoniecGry() {
         return bledneProby >= maxProb || czySlowoOdgadniete();
     }
-
+// Sprawdza, czy słowo zostało odgadnięte poprawnie
     public boolean czySlowoOdgadniete() {
         for (int i = 0; i < slowo.length(); i++) {
             if (!zgadywaneLitery.toString().contains(Character.toString(slowo.charAt(i)))) {
@@ -25,8 +26,7 @@ public class WisielecGra {
         }
         return true;
     }
-
-
+   // Odgaduje literę
     public boolean zgadnijLitera(char litera) {
         boolean czyPoprawna = slowo.contains(Character.toString(litera));
         if (!zgadywaneLitery.toString().contains(Character.toString(litera))) {
@@ -38,7 +38,7 @@ public class WisielecGra {
         return czyPoprawna;
     }
 
-
+ // Zwraca słowo z odgadniętymi literami
     public String getSlowoZOdgadnietymi() {
         StringBuilder wynik = new StringBuilder();
         for (int i = 0; i < slowo.length(); i++) {
