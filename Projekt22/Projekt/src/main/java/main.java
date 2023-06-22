588516101de80bce3d119ddfbe7126ef4059cd3a
MalgorzataBrzeska <137276852+MalgorzataBrzeska@users.noreply.github.com>
2023-06-22 11:24:23 +0200

    main.java

--- Projekt22/Projekt/src/main/java/main.java
+++ Projekt22/Projekt/src/main/java/main.java
@@ -5,11 +5,12 @@ import interfejs.WisielecInterfejs;
 
 public class Wisielecmain {
     public static void main(String[] args) {
-        String slowo = "wisielec";
-        int maxProb = 6;
+        String slowo = "wisielec"; // Wybrane słowo do odgadnięcia
+        int maxProb = 6; // Maksymalna liczba błędnych prób
+
 
         WisielecGra wisielecGra = new WisielecGra(slowo, maxProb);
         WisielecInterfejs wisielecInterfejs = new WisielecInterfejs(wisielecGra);
-        wisielecInterfejs.rozpocznijGre();
+        wisielecInterfejs.rozpocznijGre(); // Rozpoczęcie gry
     }
 }
